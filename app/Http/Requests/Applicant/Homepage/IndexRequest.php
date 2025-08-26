16076a565a5f34c599e8d88f2633b5e0280b3596
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-08-26 15:44:14 +0700

    Create IndexRequest.php

---
+++ app/Http/Requests/Applicant/Homepage/IndexRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Applicant\Homepage;
+
+use App\Enums\PostRemotableEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class IndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'cities' => [
+                'array',
+                'nullable',
+            ],
+            'min_salary' => [
+                'integer',
+            ],
+            'max_salary' => [
+                'integer',
+            ],
+            'remotable' => [
+                'nullable',
+                Rule::in(PostRemotableEnum::asArray()),
+            ]
+        ];
+    }
+}
